6b0e6e589d19f087f1adb3eece0f3e27a31c4e39
jancephei <acephei@gmail.com>
2018-08-17 10:14:01 -0500

    Update and rename README.md to RecoveryComm.h

---
+++ RecoveryComm.h
@@ -0,0 +1,116 @@
+// the communication ESP8266WiFi with firebase is too easy 
+// i can see a lot of codes about the communication
+// but i have some problems when when have passed some seconds or minutes and try to send or get some values to firebase
+// the solution is too easy only need validate again the internet connection
+// I use the firebaseDemo like code base, included in the librerys for this example
+// and i change the wifi connection in a method
+//
+// Another thing i want to said, recently the firebase change the FirebaseFingerprint and if you try to comunicate with fire base you will have problems.
+// only need update the file FirebaseHttpClient.h and the value kFirebaseFingerprint
+// the new value is: "6F D0 9A 52 C0 E9 E4 CD A0 D3 02 A4 B7 A1 92 38 2D CA 2F 26";
+// i hope this code can help in your projects
+
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "example.firebaseio.com"
+#define FIREBASE_AUTH "token_or_secret"
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
+void wifiConnect(){
+
+	while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+ 
+	// call the method
+  wifiConnect();
+ 
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+int n = 0;
+
+void loop() {
+  //before send or get value, call the method  to check the wifi status
+  wifiConnect();
+
+
+ // set value
+  Firebase.setFloat("number", 42.0);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+  
+  // update value
+  Firebase.setFloat("number", 43.0);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+
+  // get value 
+  Serial.print("number: ");
+  Serial.println(Firebase.getFloat("number"));
+  delay(1000);
+
+  // remove value
+  Firebase.remove("number");
+  delay(1000);
+
+  // set string value
+  Firebase.setString("message", "hello world");
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /message failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+  
+  // set bool value
+  Firebase.setBool("truth", false);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /truth failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+
+  // append a new value to /logs
+  String name = Firebase.pushInt("logs", n++);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("pushing /logs failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  Serial.print("pushed: /logs/");
+  Serial.println(name);
+  
+  // change the dalay 10 seconds
+  delay(10000);
+}
